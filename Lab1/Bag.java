dbce75e629ca0ed234e5832a0e5a4793af3b1967
Diego Magdaleno <dmmagdal@ucsc.edu>
2017-06-16 23:03:08 -0700

    added documentation to Lab1 programs

--- Lab1/Bag.java
+++ Lab1/Bag.java
@@ -8,11 +8,17 @@ class Bag{
 	public int[] bag;
 	private int bagSize;
 
+	/** Intitailize a new Bag class
+	@parem takes the size of the bag to create the int array of that size
+	@return returns nothing**/
 	public Bag(int size){
 		bagSize = size;
 		bag = new int[size];
 	}
 
+	/** Intitailize a new Bag class
+	@parem takes the size of the bag to create the int array of that size
+	@return returns nothing**/
 	public void toArray(int[] array){
 		if (array.length != bagSize){
 			System.out.println("Error: Array bounds are not the same as bag size.");
@@ -22,6 +28,9 @@ class Bag{
 		}
 	}
 
+	/** checks the bag and otherArray to see if they're equal but checking that they're both of same size and have the same entries
+	@parem takes the int[] otherArray as an argument to see if it and the bag are equal
+	@return returns true if the bag and otherArray are equal and false if the bag and otherArray are not equal**/
 	public boolean equals(int[] otherArray){
 		boolean equal = false;
 		if (bagSize == otherArray.length && sameEntries(otherArray)){
@@ -30,6 +39,9 @@ class Bag{
 		return equal;
 	}
 
+	/** runts through both the bag and otherArray to see if they both only eachother's entries
+	@parem takes the int[] otherArray as an argument to check if it and the bag have the same entries
+	@return returns true if both the bag and otherArray have the same entries and false if the bag and otherArray don't have the same entries**/
 	private boolean sameEntries(int[] otherArray){
 		boolean same = true;
 		for (int index = 0; index < bagSize; index++){
@@ -40,6 +52,9 @@ class Bag{
 		return same;
 	}
 
+	/** checks to see if an entry is within an array
+	@parem takes the int entry as a query and int[] otherArray to see if the query exists in the array
+	@return returns false if the entry is not in otherArray and true if entry is in otherArray**/
 	private boolean contains(int entry, int[] otherArray){
 		boolean found = false;
 		int index = 0;
--- Lab1/BagTest.java
+++ Lab1/BagTest.java
@@ -6,24 +6,28 @@
 
 class BagTest{
 	public static void main(String[] args){
+		// initialize new bags
 		Bag a = new Bag(3);
 		Bag b = new Bag(5);
 		Bag c = new Bag(5);
 		Bag d = new Bag(5);
 		Bag e = new Bag(3);
 
+		// create fixed arrays
 		int[] aPrime = {5, 2, 1};
 		int[] bPrime = {7, 9, 10, 2, 2};
 		int[] cPrime = {10, 2, 7, 2, 9};
 		int[] dPrime = {10, 9, 1, 2, 1};
 		int[] ePrime = {1, 5, 2};
 
+		// set the bags equal to the arrays made above
 		a.toArray(aPrime);
 		b.toArray(bPrime);
 		c.toArray(cPrime);
 		d.toArray(dPrime);
 		e.toArray(ePrime);
 
+		// print out the bags
 		System.out.println("A: ");
 		for (int i = 0; i < a.bag.length; i++){
 			System.out.print(a.bag[i]+" ");
@@ -47,7 +51,7 @@ class BagTest{
 
 		System.out.println("\n");
 
-
+		// print the comparison between each bag
 		System.out.println("A and B are equal: "+a.equals(b.bag));
 		System.out.println("A and C are equal: "+a.equals(c.bag));
 		System.out.println("B and C are equal: "+b.equals(c.bag));
